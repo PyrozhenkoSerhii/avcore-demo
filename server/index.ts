f69acac8cca93268758739fd42f9a047403702f9
SerhiiPyrozhenko <serhii.pyrozhenko@nure.ua>
2020-08-20 11:54:57 +0300

    revert server back

--- server/index.ts
+++ server/index.ts
@@ -32,54 +32,54 @@ const app = express();
 
 const clientDistFolder = process.env.FROM_BUILD ? "../../client/dist" : "../client/dist";
 
-const httpsServer = createServer(credentials, app);
-
-// const io = socketIO(httpsServer);
-
-// io.on("connection", (socket: IExtSocket) => {
-//   console.log("> Socket connection was established");
-
-//   socket.on("auth", (data: TOnAuthProps, callback: TOnAuthCallback) => {
-//     const {operation, stream} = data;
-//     const token = jwt.sign({ stream, operation }, SECRET, { expiresIn: getExpiration(), algorithm: "HS512" });
-
-//     console.log(`> Created token for stream: ${stream}, operation: ${operation}`);
-
-//     callback(token);
-//   });
-
-//   socket.on("save_mixer", (data: TOnSaveMixerProps) => {
-//     const { mixerId, serverUrl, streamId } = data;
-
-//     socket.mixerId = mixerId;
-//     socket.streamId = streamId;
-//     socket.serverUrl = serverUrl;
-//   });
-
-//   socket.on("disconnect", () => {
-//     console.log("> Socket got disconnected!");
-
-//     if(socket.mixerId) {
-//       console.log("> Closing the mixer...");
-//       const token = jwt.sign(
-//         { stream: socket.streamId, operation: API_OPERATION.MIXER },
-//         SECRET,
-//         { expiresIn: getExpiration(), algorithm: "HS512" });
-  
-//       const api = new MediasoupSocketApi(socket.serverUrl, 0, token);
-  
-//       api.mixerClose({ mixerId:socket.mixerId });
-//     }
-//   });
-// });
-
-
-httpsServer.listen(PORT, () => {
-  console.log(`> HTTPS Server running on port ${PORT}`);
-});
-
 app.use(cors());
 app.use(express.static(path.join(__dirname, clientDistFolder)));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, clientDistFolder, "index.html"));
+});
+
+const httpsServer = createServer(credentials, app);
+
+const io = socketIO(httpsServer);
+
+io.on("connection", (socket: IExtSocket) => {
+  console.log("> Socket connection was established");
+
+  socket.on("auth", (data: TOnAuthProps, callback: TOnAuthCallback) => {
+    const {operation, stream} = data;
+    const token = jwt.sign({ stream, operation }, SECRET, { expiresIn: getExpiration(), algorithm: "HS512" });
+
+    console.log(`> Created token for stream: ${stream}, operation: ${operation}`);
+
+    callback(token);
+  });
+
+  socket.on("save_mixer", (data: TOnSaveMixerProps) => {
+    const { mixerId, serverUrl, streamId } = data;
+
+    socket.mixerId = mixerId;
+    socket.streamId = streamId;
+    socket.serverUrl = serverUrl;
+  });
+
+  socket.on("disconnect", () => {
+    console.log("> Socket got disconnected!");
+
+    if(socket.mixerId) {
+      console.log("> Closing the mixer...");
+      const token = jwt.sign(
+        { stream: socket.streamId, operation: API_OPERATION.MIXER },
+        SECRET,
+        { expiresIn: getExpiration(), algorithm: "HS512" });
+  
+      const api = new MediasoupSocketApi(socket.serverUrl, 0, token);
+  
+      api.mixerClose({ mixerId:socket.mixerId });
+    }
+  });
+});
+
+
+httpsServer.listen(PORT, () => {
+  console.log(`> HTTPS Server running on port ${PORT}`);
 });
